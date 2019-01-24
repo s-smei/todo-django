0b49946511d23e23d3c5864eea07f5372dfd4564
Serafym Smei <serafym.smei@gmail.com>
2019-01-24 22:27:12 +0100

    Added form to add new todo from user home page. Added del option for each todo.

--- player/views.py
+++ player/views.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from list.models import Todo
+from list.models import List
+from list.forms import ListForm
 # Create your views here.
 
 
 @login_required()
 def home(request):
-    my_todos = Todo.objects.filter(owner=request.user)
-    active_todos = my_todos
-    finished_todos = []
+    my_lists = List.objects.filter(owner=request.user)
+    active_lists = my_lists
+    finished_lists = []
+
+    form = ListForm()
     return render(request, "player/home.html",
-                  {'active_todos': active_todos,
-                   'finished_todos': finished_todos})
+                  {'active_todos': active_lists,
+                   'finished_todos': finished_lists,
+                   'form': form}
+                  )
+
